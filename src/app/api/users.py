54e1d83ab57f37661d03ba427fc00b352b9bab02
jossefaz <jossefaz@protonmail.com>
2021-03-20 22:03:59 +0200

    add API KEY

--- src/app/api/users.py
+++ src/app/api/users.py
@@ -1,15 +1,41 @@
+import os
 from typing import List
 
 from ..db import users as user_repository
 from .schemas import User, PublicUser
-from fastapi import APIRouter, HTTPException, Path, status, Depends
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi import APIRouter, HTTPException, status, Security, Depends
+from fastapi.security.api_key import APIKeyQuery, APIKeyCookie, APIKeyHeader, APIKey
 
+
+API_KEY = os.getenv("API_KEY")
+API_KEY_NAME = os.getenv("API_KEY_NAME")
+
+api_key_query = APIKeyQuery(name=API_KEY_NAME, auto_error=False)
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+api_key_cookie = APIKeyCookie(name=API_KEY_NAME, auto_error=False)
+
+
+async def get_api_key(
+    api_key_query: str = Security(api_key_query),
+    api_key_header: str = Security(api_key_header),
+    api_key_cookie: str = Security(api_key_cookie),
+):
+
+    if api_key_query == API_KEY:
+        return api_key_query
+    elif api_key_header == API_KEY:
+        return api_key_header
+    elif api_key_cookie == API_KEY:
+        return api_key_cookie
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials"
+        )
 router = APIRouter()
 
 
 @router.post("/", response_model=PublicUser, status_code=status.HTTP_201_CREATED)
-async def create_user(payload: User):
+async def create_user(payload: User, api_key: APIKey = Depends(get_api_key)):
     user_id = await user_repository.create(payload)
     user = await user_repository.get_one(user_id)
     if not user:
@@ -19,7 +45,7 @@ async def create_user(payload: User):
 
 
 @router.get("/{id}", response_model=PublicUser, status_code=status.HTTP_200_OK)
-async def get_user(id: int):
+async def get_user(id: int, api_key: APIKey = Depends(get_api_key)):
     user = await user_repository.get_one(id)
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The user with id {id} was not found")
@@ -27,7 +53,7 @@ async def get_user(id: int):
 
 
 @router.post("/credentials", status_code=status.HTTP_200_OK)
-async def check_user_credentials(request: User):
+async def check_user_credentials(request: User, api_key: APIKey = Depends(get_api_key)):
     valid_credentials = await user_repository.check_credentials(request)
     if not valid_credentials:
         raise HTTPException(
--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import pytest
 from fastapi import status
@@ -10,19 +11,25 @@ from ..app.api.schemas import User
 
 
 @pytest.mark.parametrize(
-    "payload, status_code",
+    "payload, status_code, api_key",
     [
-        [{"email": "john", "password": "secret"}, status.HTTP_201_CREATED],
-        [{"email": "john"}, status.HTTP_422_UNPROCESSABLE_ENTITY],
-        [{"password": "secret"}, status.HTTP_422_UNPROCESSABLE_ENTITY],
-        [{"password": "4let"}, status.HTTP_422_UNPROCESSABLE_ENTITY]
+        [{"email": "john", "password": "secret"}, status.HTTP_201_CREATED, os.getenv("API_KEY")],
+        [{"email": "john", "password": "secret"}, status.HTTP_401_UNAUTHORIZED, None],
+        [{"email": "john"}, status.HTTP_422_UNPROCESSABLE_ENTITY, os.getenv("API_KEY")],
+        [{"password": "secret"}, status.HTTP_422_UNPROCESSABLE_ENTITY, os.getenv("API_KEY")],
+        [{"password": "4let"}, status.HTTP_422_UNPROCESSABLE_ENTITY, os.getenv("API_KEY")]
     ]
 )
-def test_create_user(test_app: TestClient, monkeypatch, payload, status_code):
+def test_create_user(test_app: TestClient, monkeypatch, payload, status_code, api_key):
     async def mock_create(user):
         return 1
 
+    async def mock_get_one(id):
+        return {"id": 1, "email": "john", "password": "secret"}
+
+    test_app.headers[os.getenv("API_KEY_NAME")] = api_key
     monkeypatch.setattr(user_repository, "create", mock_create)
+    monkeypatch.setattr(user_repository, "get_one", mock_get_one)
     response = test_app.post("/users/", data=json.dumps(payload))
     assert response.status_code == status_code
 
@@ -33,6 +40,7 @@ def test_retrieve_user(test_app: TestClient, monkeypatch):
     async def mock_get_one(id):
         return test_user
 
+    test_app.headers[os.getenv("API_KEY_NAME")] = os.getenv("API_KEY")
     monkeypatch.setattr(user_repository, "get_one", mock_get_one)
     response = test_app.get("/users/1")
     assert response.status_code == status.HTTP_200_OK
@@ -43,6 +51,7 @@ def test_user_not_found(test_app: TestClient, monkeypatch):
     async def mock_get_one(id):
         return None
 
+    test_app.headers[os.getenv("API_KEY_NAME")] = os.getenv("API_KEY")
     monkeypatch.setattr(user_repository, "get_one", mock_get_one)
     response = test_app.get("/users/1")
     assert response.status_code == status.HTTP_404_NOT_FOUND
@@ -56,23 +65,26 @@ def test_user_hashing_password():
 
 
 @pytest.mark.parametrize(
-    "payload, status_code",
+    "payload, status_code, api_key",
     [
-        [{"email": "some_existing_user", "password": "some_existing_password"}, status.HTTP_200_OK],
-        [{"email": "john"}, status.HTTP_422_UNPROCESSABLE_ENTITY],
-        [{"email": "some_existing_user", "password": "not_corresponding_password"}, status.HTTP_401_UNAUTHORIZED],
-        [{"email": "not_existing_user", "password": "some_existing_password"}, status.HTTP_401_UNAUTHORIZED]
+        [{"email": "some_existing_user", "password": "some_existing_password"}, status.HTTP_200_OK,
+         os.getenv("API_KEY")],
+        [{"email": "john"}, status.HTTP_422_UNPROCESSABLE_ENTITY, os.getenv("API_KEY")],
+        [{"email": "some_existing_user", "password": "not_corresponding_password"}, status.HTTP_401_UNAUTHORIZED,
+         os.getenv("API_KEY")],
+        [{"email": "not_existing_user", "password": "some_existing_password"}, status.HTTP_401_UNAUTHORIZED,
+         os.getenv("API_KEY")],
+        [{"email": "not_existing_user", "password": "some_existing_password"}, status.HTTP_401_UNAUTHORIZED, None]
     ]
 )
-def test_user_credentials(test_app: TestClient, monkeypatch, payload, status_code):
-    async def mock_check_credentials(credentials:User):
+def test_user_credentials(test_app: TestClient, monkeypatch, payload, status_code, api_key):
+    async def mock_check_credentials(credentials: User):
         if "email" in payload and "password" in payload:
             if payload["email"] == "some_existing_user" and payload["password"] == "some_existing_password":
                 return True
         return False
 
     monkeypatch.setattr(user_repository, "check_credentials", mock_check_credentials)
+    test_app.headers[os.getenv("API_KEY_NAME")] = api_key
     response = test_app.post("/users/credentials", data=json.dumps(payload))
     assert response.status_code == status_code
-
-
