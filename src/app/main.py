e6d0c0de3a91050734f6bf09aa221a98b649cd8a
jossefaz <jossefaz@protonmail.com>
2021-03-22 12:10:04 +0200

    add cors

--- src/app/main.py
+++ src/app/main.py
@@ -1,3 +1,4 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 from .api import monitor, users
@@ -6,7 +7,16 @@ from .db.db_models import metadata
 
 metadata.create_all(engine)
 
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event("startup")
