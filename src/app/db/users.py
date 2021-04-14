2a37c81009ef5384f7798f6df9cba9b46ffc53a7
jossefaz <jossefaz@protonmail.com>
2021-04-14 18:40:40 +0300

    add log midellware

--- src/app/db/users.py
+++ src/app/db/users.py
@@ -1,31 +1,37 @@
-from fastapi.security import OAuth2PasswordRequestForm
-
 from ..api.schemas import User
 from .db_models import Users
 from .db_engine import database
 from ..utils import hashing
+from ..utils.logs import log_sql_query
 
 
 async def create(user: User):
     query = Users.insert().values(username=user.username, password=hashing.bcrypt(user.password))
+    log_sql_query(query)
     return await database.execute(query=query)
 
 
-async def get_one(id: int):
-    query = Users.select().where(id == Users.c.id)
-    return await database.fetch_one(query=query)
+async def get_one(user_id: int):
+    query = Users.select().where(user_id == Users.c.id)
+    user_found = await database.fetch_all(query=query)
+    log_sql_query(sql_query=query, record_num=len(user_found))
+    return user_found[0] if len(user_found) > 0 else None
+
 
 async def get_one_by_name(username: str):
     query = Users.select().where(username == Users.c.username)
-    return await database.fetch_one(query=query)
+    user_found = await database.fetch_all(query=query)
+    log_sql_query(sql_query=query, record_num=len(user_found))
+    return user_found[0] if len(user_found) > 0 else None
+
 
 async def check_credentials(user: User):
     query = Users.select().where(Users.c.username == user.username)
-    exists = await database.fetch_one(query=query)
-    if not exists:
+    user_found = await database.fetch_all(query=query)
+    log_sql_query(sql_query=query, record_num=len(user_found))
+    if not len(user_found) == 0:
         return False
-    password_ok = hashing.verify(exists.get("password"), user.password)
-    print(password_ok)
+    password_ok = hashing.verify(user_found[0].get("password"), user.password)
     if not password_ok:
         return False
-    return exists
+    return user_found
